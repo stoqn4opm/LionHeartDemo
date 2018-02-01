729485bcbf976bff33cdf05561b7fbf15ef705da
Stoyan Stoyanov <stoqn.stoqnov.93@gmail.com>
2018-02-01 11:01:54 +0100

    [EditScreen] ui of edit panel finished

--- LionHeartDemo/Screens/Edit Photo Screen/EditPhotoViewController.swift
+++ LionHeartDemo/Screens/Edit Photo Screen/EditPhotoViewController.swift
@@ -10,14 +10,36 @@ import UIKit
 
 class EditPhotoViewController: UIViewController {
 
+    @IBOutlet weak var editContainerBottomConstaint: NSLayoutConstraint!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    @IBOutlet weak var editContainer: UIVisualEffectView!
     @IBOutlet weak var imageView: UIImageView!
     var viewModel: EditPhotoViewModel?
+    
+    fileprivate var isInEditMode: Bool? {
+        didSet {
+            editContainerBottomConstaint.constant = isInEditMode == true ? -60 : -200
+            view.setNeedsUpdateConstraints()
+            editButton.title = isInEditMode == true ? "Cancel".localized : "Edit".localized
+            saveButton.isEnabled = isInEditMode == true
+            UIView.animate(withDuration: 1.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .allowUserInteraction, animations: {[weak self] in
+                
+                self?.view.layoutIfNeeded()
+                self?.editContainer.isUserInteractionEnabled = self?.isInEditMode == true
+
+            }, completion: nil)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
         imageView.image = viewModel?.photo?.image
         title = viewModel?.photo?.title
+        isInEditMode = false
+        editContainer.layer.cornerRadius = 9
+        editContainer.clipsToBounds = true
     }
 }
 
@@ -28,11 +50,13 @@ extension EditPhotoViewController {
         
         guard let image = viewModel?.photo?.image else { return }
         guard let title = viewModel?.photo?.title else { return }
+        isInEditMode = false
         
         let shareController = UIActivityViewController(activityItems: [image, title], applicationActivities: nil)
         present(shareController, animated: true, completion: nil)
     }
     
     @IBAction func editButtonTapped(_ sender: UIBarButtonItem) {
+        isInEditMode = isInEditMode == true ? false : true
     }
 }
